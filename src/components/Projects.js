b1d22db03d769406453f27fd52e1d47b4f8f0c26
Prinyavong Phongsavan <pphongsavan@volute.co>
2023-02-13 22:27:41 -0500

    Updated tip calculator image and link.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,44 +1,37 @@
-import Project from "./Project"
+import Project from "./Project";
 
 const Projects = () => {
+  const projects = [
+    {
+      dataContent: "Tip Calculator",
+      link: "https://ptp-calculators.netlify.app/",
+      id: "tip-link",
+      key: "tip-link",
+      alt: "Tip Calculator",
+      image: "./img/tip.png",
+    },
+    {
+      dataContent: "JoJo's Stand Name Mixer",
+      link: "https://pphongsavan.github.io/jojo-stand-name-mixer/",
+      id: "jojo-link",
+      key: "jojo-link",
+      alt: "JoJo's Stand Name Mixer",
+      image: "./img/jojo.png",
+    },
+  ];
 
-    const projects = [
-        {
-            dataContent: "Tip Calculator",
-            link: "https://pphongsavan.github.io/tip-calculator",
-            id: "tip-link",
-            key: "tip-link",
-            alt: "Tip Calculator",
-            image: "./img/tip.png",
-        },
-        {
-            dataContent: "JoJo's Stand Name Mixer",
-            link: "https://pphongsavan.github.io/jojo-stand-name-mixer/",
-            id: "jojo-link",
-            key: "jojo-link",
-            alt: "JoJo's Stand Name Mixer",
-            image: "./img/jojo.png",
-        },
-        {
-            dataContent: "Online Letter",
-            link: "https://codepen.io/prinyesthatsme/pen/ZVEjRz",
-            id: "letter-link",
-            key: "letter-link",
-            alt: "Online Letter",
-            image: "./img/letter.png",
-        },
-    ]
+  return (
+    <section id="projects" className="section">
+      <div id="porfolioTitle" className="section-title">
+        <h2 className="bold">Projects</h2>
+      </div>
+      <div id="tiles">
+        {projects.map((project) => (
+          <Project key={project.key} projectItem={project} />
+        ))}
+      </div>
+    </section>
+  );
+};
 
-    return (
-        <section id="projects" className="section">
-            <div id="porfolioTitle" className="section-title"><h2 className="bold">Projects</h2></div>
-            <div id="tiles">
-                {projects.map((project) => (
-                    <Project key={project.key} projectItem={project} />
-                ))}
-            </div>
-        </section>
-    )
-}
-
-export default Projects
+export default Projects;
